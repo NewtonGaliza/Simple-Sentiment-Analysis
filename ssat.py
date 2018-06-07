a0d62be52c30449bb2e3fcfabd10f8dae19971da
Newton Galiza <newtonjgaliza@gmail.com>
2018-06-07 17:08:46 -0300

    Update ssat.py

--- ssat.py
+++ ssat.py
@@ -2,10 +2,10 @@ from textblob import TextBlob as tb
 import tweepy
 import numpy as np
 
-consumer_key = '2Ff1fwhV8PubQ9KRWg5BHNYVK'
-consumer_secret = 'WHfqGZMdupXib0MpvyP7gmQskRLhb1F2mo65RDYVli58SwpqAs'
-access_token = '843892947909332998-MlRVUB1H7D3xHw68eZLM2h8ONu4vkmM'
-access_token_secret = 'bGnNDoHWEFLaqO2OSLoGkMiIKR4bKopwufxaZnGuAe9ha'
+consumer_key = 'your consumer key'
+consumer_secret = 'your consumer secret'
+access_token = 'your access token'
+access_token_secret = 'your acess token secret'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -20,4 +20,4 @@ for tweet in public_tweets:
     analysis = tb(tweet.text)
     print(analysis.sentiment.polarity)
     
-print('MÉDIA DE SENTIMENTO: ' + str(np.mean(analysis.sentiment.polarity)))
+print('Average: ' + str(np.mean(analysis.sentiment.polarity)))
